fdac31968fa1112390d899b342cf2c1ed86cdc89
EmKen <emily.kennedy_95@hotmail.co.uk>
2017-05-30 17:02:18 +0800

    Live search with ajax

--- app/assets/javascripts/listings.js
+++ app/assets/javascripts/listings.js
@@ -3,4 +3,8 @@ $(function() {
 		$.getScript(this.href);
 		return false;
 	});
+	$("#search_form input").keyup(function() {
+		$.get($("#search_form").attr("action"), $("#search_form").serialize(), null, "script");
+		return false;
+	});
 });
